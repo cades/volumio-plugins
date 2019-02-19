09643fb588f531364fdad176efb31677a7f81386
cades kao <cadeskao@gmail.com>
2019-02-19 03:41:54 +0000

    manage playing state properly
    after studying volspotconnect2's code, I realized that
    I should call pushState() to get player state sync to Volumio core.
    
    also, this commit use VLC rc interface instead of http interface,
    to get easier integration.

--- plugins/music_service/streetvoice/index.js
+++ plugins/music_service/streetvoice/index.js
@@ -4,7 +4,7 @@ const libQ = require('kew');
 const config = new (require('v-conf'))();
 const { spawn } = require('child_process');
 const { getPlaylist, searchForSongs, convertSongUrlToM3u8Url, getM3u8Info, getTrackInfo } = require('./lib');
-const { play, seek, pause, resume, stop } = require('./vlc');
+const player = require('./player')();
 
 // reimplement Promise API using kew...WTF
 function Promise(fn) {
@@ -34,18 +34,43 @@ streetvoice.prototype.onStart = volumiofy(function() {
   this.logger.info('[StreetVoice] start');
   this.commandRouter.pushToastMessage('success', "Plugin Start!", "StreetVoice plugin start successfully");
   this.addToBrowseSources();
-  this.launchPlayer();
+
+  this.servicename = 'streetvoice';
+  this.displayname = 'StreetVoice';
+  this.state = {
+    status: 'stop',
+    service: this.servicename,
+    title: '',
+    artist: '',
+    album: '',
+    albumart: '/albumart',
+    uri: '',
+    trackType: 'streetvoice',
+    seek: 0,
+    duration: 0,
+    channels: 2,
+    streaming: true
+  };
+
+  player.launch();
+  player.on('stateChange', ({ position, duration, status }) => {
+    this.state.seek = position;
+    if (status === 'stopped') this.state.status = 'stop';
+    if (status === 'paused') this.state.status = 'pause';
+    if (status === 'playing') this.state.status = 'play';
+    this.pushState();
+  });
 });
 
 streetvoice.prototype.onStop = volumiofy(function() {
   this.logger.info('[StreetVoice] stop');
-  stop();
-  this.quitPlayer();
+  player.stop();
+  player.quit();
 });
 
-streetvoice.prototype.onRestart = function() {
+streetvoice.prototype.onRestart = volumiofy(function() {
   // Optional, use if you need it
-};
+});
 
 
 // Configuration Methods -----------------------------------------------------------------------------
@@ -84,7 +109,7 @@ streetvoice.prototype.setConf = function(varName, varValue) {
 // If your plugin is not a music_sevice don't use this part and delete it
 
 
-streetvoice.prototype.addToBrowseSources = function () {
+streetvoice.prototype.addToBrowseSources = volumiofy(function () {
   // Use this function to add your music service plugin to music sources
   const data = {
     name: 'StreetVoice',
@@ -94,7 +119,7 @@ streetvoice.prototype.addToBrowseSources = function () {
     albumart: '/albumart?sourceicon=music_service/streetvoice/TypeS.png'
   };
   this.commandRouter.volumioAddToBrowseSources(data);
-};
+});
 
 streetvoice.prototype.handleBrowseUri = volumiofy(async function (uri) {
   //self.commandRouter.logger.info(uri);
@@ -192,43 +217,44 @@ streetvoice.prototype.handleBrowseUri = volumiofy(async function (uri) {
 streetvoice.prototype.clearAddPlayTrack = volumiofy(async function(track) {
   this.logger.info('[streetvoice] clearAddPlayTrack');
   this.logger.info('[streetvoice] track:' + JSON.stringify(track));
-  const res = await play(track.uri);
-  return res;
+  player.play(track.uri);
 });
 
-streetvoice.prototype.seek = volumiofy(async function (timepos) {
-  this.logger.info('[' + Date.now() + '] ' + 'streetvoice::seek to ' + timepos);
-  seek(Math.round(timepos / 1000));
+streetvoice.prototype.seek = volumiofy(async function (position_ms) {
+  this.logger.info('[' + Date.now() + '] ' + 'streetvoice::seek to ' + position_ms);
+  player.seek(Math.round(position_ms / 1000));
+  this.state.seek = position_ms;
+  this.pushState();
 });
 
 // Stop
-streetvoice.prototype.stop = function() {
+streetvoice.prototype.stop = volumiofy(function() {
   const self = this;
   this.logger.info('[' + Date.now() + '] ' + 'streetvoice::stop');
-  stop();
-};
+  player.stop();
+});
 
 // Pause
-streetvoice.prototype.pause = function() {
+streetvoice.prototype.pause = volumiofy(function() {
   const self = this;
   this.logger.info('[' + Date.now() + '] ' + 'streetvoice::pause');
-  pause();
-};
+  player.pause();
+});
 
 // Pause
-streetvoice.prototype.resume = function() {
+streetvoice.prototype.resume = volumiofy(function() {
   const self = this;
   this.logger.info('[' + Date.now() + '] ' + 'streetvoice::resume');
-  resume();
-};
+  player.resume();
+});
 
 // Get state
-streetvoice.prototype.getState = function() {
+streetvoice.prototype.getState = volumiofy(function() {
   const self = this;
   this.logger.info('[' + Date.now() + '] ' + 'streetvoice::getState');
 
-
-};
+  return this.state;
+});
 
 //Parse state
 streetvoice.prototype.parseState = function(sState) {
@@ -239,12 +265,12 @@ streetvoice.prototype.parseState = function(sState) {
 };
 
 // Announce updated State
-streetvoice.prototype.pushState = function(state) {
+streetvoice.prototype.pushState = volumiofy(function(state) {
   const self = this;
   this.logger.info('[' + Date.now() + '] ' + 'streetvoice::pushState');
 
-  return self.commandRouter.servicePushState(state, self.servicename);
-};
+  return self.commandRouter.servicePushState(self.state, self.servicename);
+});
 
 
 streetvoice.prototype.explodeUri = volumiofy(async function(uri) {
@@ -253,6 +279,13 @@ streetvoice.prototype.explodeUri = volumiofy(async function(uri) {
   const m3u8Info = await getM3u8Info(m3u8Url);
   const duration = Math.round(m3u8Info.segments.reduce((total, segment) => total + segment.duration, 0));
   const trackInfo = await getTrackInfo(uri);
+
+  this.state.uri = m3u8Url;
+  this.state.duration = duration;
+  this.state.title = trackInfo.songName;
+  this.state.artist = trackInfo.artist;
+  this.state.albumart = trackInfo.albumArt;
+  this.pushState();
 
   const data = {
     uri: m3u8Url,
@@ -268,7 +301,7 @@ streetvoice.prototype.explodeUri = volumiofy(async function(uri) {
   return data;
 });
 
-streetvoice.prototype.getAlbumArt = function (data, path) {
+streetvoice.prototype.getAlbumArt = volumiofy(function (data, path) {
 
   let artist, album;
 
@@ -301,7 +334,7 @@ streetvoice.prototype.getAlbumArt = function (data, path) {
     url = url + 'path=' + nodetools.urlEncode(path);
 
   return url;
-};
+});
 
 streetvoice.prototype.search = volumiofy(async function (query) {
   // Mandatory, search. You can divide the search in sections using following functions
@@ -322,22 +355,6 @@ streetvoice.prototype.search = volumiofy(async function (query) {
     }))
   }];
 });
-
-streetvoice.prototype.launchPlayer = function () {
-  if (this.player) this.quitPlayer();
-  const audioDevice = this.getAudioDevice();
-  this.player = spawn('vlc', `-I http --http-password=test --aout=alsa --alsa-audio-device=${audioDevice}`.split(' '));
-}
-
-streetvoice.prototype.quitPlayer = function () {
-  if (!this.player) return;
-  this.player.kill();
-  this.player = null;
-}
-
-streetvoice.prototype.getAudioDevice = function () {
-  return 'default:CARD=sndrpihifiberry';
-}
 
 function volumiofy(fn) {
   return function(...args) {
---
+++ plugins/music_service/streetvoice/player.js
@@ -0,0 +1,79 @@
+const { spawn } = require('child_process')
+const EventEmitter = require('events');
+
+module.exports = () => {
+  let vlcProcess;
+  let player = new EventEmitter();
+  let intervalHandle;
+  let currState = {};
+
+  function launch() {
+    if (vlcProcess) quitPlayer();
+    vlcProcess = spawn('vlc', ['-I', 'rc', '--no-repeat', '--no-loop', '--aout=alsa', '--alsa-audio-device=default:CARD=sndrpihifiberry']);
+
+    vlcProcess.stdout.on('data', buf => {
+      const data = buf.toString();
+      const parsed = data.split('\n').map(x => x.replace('\r', '')).filter(x => x !== '>');
+
+      if (data.startsWith('VLC media player')) return;
+      if (parsed.length < 2) return;
+
+      const duration = +parsed[0];
+      const position = +parsed[1];
+      const status = parsed.find(str => str.includes('state')).split(' ')[2];
+
+      const newState = { duration, position, status };
+      if (JSON.stringify(newState) === JSON.stringify(currState)) return;
+      currState = newState;
+      player.emit('stateChange', newState);
+    });
+
+    intervalHandle = setInterval(() => {
+      vlcProcess.stdin.write('get_length\nget_time\nstatus\n', 'utf8');
+    }, 200);
+  }
+
+  function play(m3u8Url) {
+    vlcProcess.stdin.write(`add ${m3u8Url}\nplay\n`, 'utf8');
+  }
+
+  function togglePause() {
+    vlcProcess.stdin.write('pause\n', 'utf8');
+  }
+
+  function pause() {
+    if (currState.status === 'playing') {
+      togglePause();
+    }
+  }
+
+  function resume() {
+    vlcProcess.stdin.write('play\n', 'utf8');
+  }
+
+  function stop() {
+    vlcProcess.stdin.write('stop\n', 'utf8');
+  }
+
+  function seek(n) {
+    vlcProcess.stdin.write(`seek ${n}\n`, 'utf8');
+  }
+
+  function quitPlayer() {
+    if (!vlcProcess) return;
+    clearInterval(intervalHandle);
+    vlcProcess.kill();
+    vlcProcess = null;
+  }
+
+  player.launch = launch;
+  player.play = play;
+  player.togglePause = togglePause;
+  player.pause = pause;
+  player.resume = resume;
+  player.stop = stop;
+  player.seek = seek;
+  player.quit = quitPlayer;
+
+  return player;
+};
